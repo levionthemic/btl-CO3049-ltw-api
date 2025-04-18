6271f1dd2612d88fc34d3f1f730304dae392ed10
levionthemic <liem.levicoding834124@gmail.com>
2025-04-18 16:28:39 +0700

    Successfully connect to Aiven Cloud

--- config/database.php
+++ config/database.php
@@ -1,12 +1,32 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'room_rental'; //Change from room-rental to room_rental
-$username = "root";
-$password = "";
+require_once 'environment.php';
+loadEnv();
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+
+$options = [
+  PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/../ca.pem',
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
 try {
-  $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn = new PDO(
+    "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+    $username,
+    $password,
+    $options
+  );
+  echo json_encode(["message" => "DB Connection Successful!"]);
 } catch (PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+  echo json_encode([
+    "status" => "error",
+    "message" => "DB Connection failed: " . $e->getMessage()
+  ]);
+  exit;
 }
